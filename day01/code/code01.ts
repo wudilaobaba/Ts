dd66390706847f115c58aec8e8b64f649e195235
whj <1273803935@qq.com>
2020-06-08 20:38:22 +0800

    01

---
+++ day01/code/code01.ts
@@ -0,0 +1,5 @@
+function greeter(person){
+  return 'Hello ' + person;
+}
+let user = 'Yee';
+console.log(greeter(user))
---
+++ day01/code/code02.ts
@@ -0,0 +1,5 @@
+function greeter(person: string){
+  return 'Hello ' + person;
+}
+let user = 'Yee';
+console.log(greeter(user))
