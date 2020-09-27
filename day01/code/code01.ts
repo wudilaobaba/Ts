9e6fda1a6bd7d4ef9271de5cb0bbab1ffc382283
wudilaobaba <1273803935@qq.com>
2020-09-27 21:11:56 +0800

    a

--- day01/code/code01.ts
+++
@@ -1,5 +0,0 @@
-function greeter(person){
-  return 'Hello ' + person;
-}
-let user = 'Yee';
-console.log(greeter(user))
--- day08/axioxProject/ts-axios/src/xhr.ts
+++
@@ -1,8 +0,0 @@
-import {AxiosRequestConfig} from "./types";
-
-export default function xhr(config:AxiosRequestConfig):void {
-  const {data = null,url,method = "get"} = config;//等号就是默认值
-  const request = new XMLHttpRequest();
-  request.open(method.toUpperCase(),url,true);
-  request.send(data);//body体中的数据
-}
